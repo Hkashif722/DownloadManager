37c6b440edcc41b373e35429f8080e6e63daa363
Kashif Hussain <kashif.hussain@enthral.ai>
2025-05-10 18:16:56 +0530

    Initial Commit

---
+++ CourseDownloadDemo/CourseDownloadDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseDownloadDemoApp.swift
+//  CourseDownloadDemo
+//
+//  Created by Kashif Hussain on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CourseDownloadDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
